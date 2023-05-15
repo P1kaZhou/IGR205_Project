9ee0352834e8a7a0cc0559cefe04a6849a24ba3a
Daniel Zhou <daniel.zhou.fr@gmail.com>
2023-05-15 15:30:14 +0200

    pipeline definitions done

--- src/base/modeling/pipeline.hpp
+++ src/base/modeling/pipeline.hpp
@@ -7,6 +7,14 @@ class Pipeline {
 public:
     Pipeline();
 
+    std::vector<Mesh> computeCylinders();
+
+    Mesh computeMeshFromCylinders(std::vector<Mesh>);
+
+    Rigging computeSkeleton(Mesh);
+
+    void pipelineStep();
+
 private:
     std::vector<glm::vec2> sketchPoints;
 
